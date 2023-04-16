df1923862ad4a27990161512a7101463045432c9
roma <roma@haservices.srv.lan>
2023-04-16 22:34:35 +0300

    modified:   animated-background.js

--- dist/animated-background.js
+++ dist/animated-background.js
@@ -516,8 +516,9 @@ function renderBackgroundHTML() {
       `;
 
       var html_element = document.querySelector("html");
-      html_element.style.removeProperty ('--app-header-background-color');
       html_element.style.removeProperty ('--primary-color');
+      html_element.style.removeProperty ('--app-header-background-color');
+
 
       var div = document.createElement("div");
       div.id = "background-video";
