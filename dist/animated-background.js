96262764bad572c81c31b047ef57b2a44f116033
roma <roma@haservices.srv.lan>
2023-04-16 21:10:30 +0300

    modified:   animated-background.js

--- dist/animated-background.js
+++ dist/animated-background.js
@@ -521,7 +521,7 @@ function renderBackgroundHTML() {
 	    html_element.style.backgroundColor = null;
 	`;
 //      var Header = document.querySelector("home-assistant-main");
-      Header.insertAdjacentHTML('afterbegin',html_element);
+      Header.insertAdjacentHTML('afterbegin',header_js);
 
       var div = document.createElement("div");
       div.id = "background-video";
